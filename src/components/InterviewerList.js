66382118cea32a71bd0a2293678525de79c75447
jiyuming <ji.yuming@hotmail.com>
2021-10-22 13:17:49 -0400

    Appoint component done

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -7,14 +7,15 @@ export default function InterviewerList(props) {
   // interviewers:array - an array of objects containing the information of each interviewer
   // interviewer:number - the id of an interviewer
   // setInterviewer:function - a function that accepts an interviewer id
+  
   const interviewers = props.interviewers.map(interviewer => {
     return (
       <InterviewerListItem
         key={interviewer.id}
         name={interviewer.name}
         avatar={interviewer.avatar}
-        selected={interviewer.id === props.interviewer}
-        // selected={interviewer.id === props.value}
+        // selected={interviewer.id === props.interviewer}
+        selected={interviewer.id === props.value}
         // setInterviewer={e => props.setInterviewer(interviewer.id)}
         setInterviewer={e => props.onChange(interviewer.id)}
       />
